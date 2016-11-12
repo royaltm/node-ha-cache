141dc9d37e05e6333f9ca5573b005299b6de1a30
Rafał Michalski <royaltm75@gmail.com>
2016-11-12 18:20:12 +0100

    make public client and server classess and namespaced debug

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+ *  Copyright (c) 2016 Rafał Michalski <royal@yeondir.com>
+ *  License: LGPL
+ */
+"use strict";
+
+exports.ZmqBaseSocket = require('./lib/zmq_base_socket')
+exports.ZmqDealerSocket = require('./lib/zmq_dealer_socket');
+exports.ZmqReqSocket = require('./lib/zmq_req_socket');
+
+exports.CacheClient = require('./lib/cache_client');
---
+++ server.js
@@ -0,0 +1,8 @@
+/* 
+ *  Copyright (c) 2016 Rafał Michalski <royal@yeondir.com>
+ *  License: LGPL
+ */
+"use strict";
+
+exports.StatesMapEmitter = require('./lib/emitter_states_map');
+exports.Cache = require('./lib/cache');
