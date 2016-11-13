452bce1947911320a9a6dacdd0c58788943d28f0
Rafał Michalski <royaltm75@gmail.com>
2016-11-13 09:32:11 +0100

    expose storages

---
+++ storage.js
@@ -0,0 +1,8 @@
+/* 
+ *  Copyright (c) 2016 Rafał Michalski <royal@yeondir.com>
+ *  License: LGPL
+ */
+"use strict";
+
+exports.LevelDownStorage = require('./lib/storage/leveldown');
+exports.TestStorage = require('./lib/storage/test');
